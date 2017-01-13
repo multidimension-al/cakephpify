336c588e7c6bfdf81bd8b9e794dec9fcf9da012b
AJ Quick <ajquick@gmail.com>
2017-01-12 19:40:39 -0800

    Create ShopifyAuthAuthenticateTest.php
    Create skeleton code.

---
+++ tests/TestCase/Auth/ShopifyAuthAuthenticateTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * CakePHPify : CakePHP Plugin for Shopify API Authentication
+ * Copyright (c) Multidimension.al (http://multidimension.al)
+ * Github : https://github.com/multidimension-al/cakephpify
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE file
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     (c) Multidimension.al (http://multidimension.al)
+ * @link          https://github.com/multidimension-al/cakephpify CakePHPify Github
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+ 
+namespace Multidimensional\Shopify\Tests\Auth;
+
+use Multidimensional\Shopify\Auth\ShopifyAuthAuthenticate;
+
+use Cake\TestSuite\TestCase;
+
+class ShopifyAuthAuthenticateTest extends TestCase {
+
+    public function setUp() {
+        parent::setUp();
+    }
+ 
+}
