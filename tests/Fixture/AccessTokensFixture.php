99d5612844fe98ef4a64757949dbea3b5221caed
AJ Quick <ajquick@gmail.com>
2017-01-13 23:07:07 -0800

    Update AccessTokensFixtures

--- tests/Fixture/AccessTokensFixture.php
+++ tests/Fixture/AccessTokensFixture.php
@@ -24,8 +24,8 @@ class AccessTokensFixture extends TestFixture {
         'shop_id' => ['type' => 'integer', 'length' => 10, 'null' => false],
         'api_key' => ['type' => 'string', 'length' => 32, 'null' => false],
         'token' => ['type' => 'string', 'length' => 255, 'null' => false],
-        'created_at' => ['type' => 'datetime', 'default' => 'CURRENT_TIMESTAMP'],
-        'updated_at' => ['type' => 'datetime', 'default' => 'CURRENT_TIMESTAMP'],
+        'created_at' => ['type' => 'datetime', 'default' => 'CURRENT_TIMESTAMP', 'null' => false],
+        'updated_at' => ['type' => 'datetime', 'default' => 'CURRENT_TIMESTAMP', 'null' => false],
         'expired_at' => ['type' => 'datetime', 'null' => true],
         '_constraints' => [
             'PRIMARY' => ['type' => 'primary', 'columns' => ['id']],
