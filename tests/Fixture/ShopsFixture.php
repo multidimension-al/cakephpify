5745797fff9cdb01dc7c31bbf1f02397ca561836
AJ Quick <ajquick@gmail.com>
2017-01-13 21:28:26 -0800

    Update ShopsFixture.php
    [skip ci]

--- tests/Fixture/ShopsFixture.php
+++ tests/Fixture/ShopsFixture.php
@@ -20,6 +20,57 @@ use Cake\TestSuite\Fixture\TestFixture;
 class ShopsFixture extends TestFixture
 {
 
-    public $connection = 'test';
+    public $fields = [
+        'id' => ['type' => ''],
+        'domain' => ['type' => ''],
+        'name' => ['type' => ''],
+        'email' => ['type' => ''],
+        'shop_owner' => ['type' => ''],
+        'address1' => ['type' => ''],
+        'address2' => ['type' => ''],
+        'city' => ['type' => ''],
+        'province_code' => ['type' => ''],
+        'province' => ['type' => ''],
+        'zip' => ['type' => ''],
+        'country' => ['type' => ''],
+        'country_code' => ['type' => ''],
+        'country_name' => ['type' => ''],
+        'source' => ['type' => ''],
+        'phone' => ['type' => ''],
+        'created_at' => ['type' => ''],
+        'updated_at' => ['type' => ''],
+        'customer_email' => ['type' => ''],
+        'latitude' => ['type' => ''],
+        'longitude' => ['type' => ''],
+        'primary_location_id' => ['type' => ''],
+        'primary_locale' => ['type' => ''],
+        'currency' => ['type' => ''],
+        'iana_timezone' => ['type' => ''],
+        'money_format' => ['type' => ''],
+        'money_with_currency_format' => ['type' => ''],
+        'taxes_included' => ['type' => ''],
+        'tax_shipping' => ['type' => ''],
+        'country_taxes' => ['type' => ''],
+        'plan_display_name' => ['type' => ''],
+        'plan_name' => ['type' => ''],
+        'has_discounts' => ['type' => ''],
+        'has_gift_cards' => ['type' => ''],
+        'myshopify_domain' => ['type' => ''],
+        'google_apps_domain' => ['type' => ''],
+        'google_apps_login_enabled' => ['type' => ''],
+        'money_in_emails_format' => ['type' => ''],
+        'money_with_currency_in_emails_format' => ['type' => ''],
+        'eligible_for_payments' => ['type' => ''],
+        'requires_extra_payments_agreement' => ['type' => ''],
+        'password_enabled' => ['type' => ''],
+        'has_storefont' => ['type' => ''],
+        'eligible_for_card_reader_giveaway' => ['type' => ''],
+        'finances' => ['type' => ''],
+        'setup_required' => ['type' => ''],
+        'force_ssl' => ['type' => ''],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id']]
+         ]
+    ];
       
 }
