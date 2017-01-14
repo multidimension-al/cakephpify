6a2ab67601b871d7fc4dcae97cbfd1930959e226
AJ Quick <ajquick@gmail.com>
2017-01-13 20:39:00 -0800

    Update Connection

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -69,10 +69,11 @@ Configure::write('App', [
 Plugin::load('Multidimensional/Shopify', ['path' => ROOT]);
 
 if (!getenv('DB_DSN')) {
-    putenv('DB_DSN=mysql://root:password@localhost/my_database');
+    putenv('DB=mysql DB_DSN=mysql://root:password@localhost/my_database');
 }
 
 ConnectionManager::config('test', [
+    'className' => 'Cake\Database\Connection',
     'url' => getenv('DS_DSN'),
     'timezone' => 'UTC'
 ]);
