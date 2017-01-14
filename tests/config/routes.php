dbd98d775d11e6ddad6d0c78d37e4f332750fb9b
AJ Quick <ajquick@gmail.com>
2017-01-14 09:48:48 -0800

    Update routes.php
    [skip ci]
    
    Remove all code, as it *may* not be needed.

--- tests/config/routes.php
+++ tests/config/routes.php
@@ -1,9 +1,1 @@
 <?php
-namespace Multidimensional\Shopify\Test\App\Config;
-
-use Cake\Routing\Router;
-
-Router::scope('/', function ($routes) {
-    $routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'DashedRoute']);
-    $routes->connect('/:controller/:action/*', [], ['routeClass' => 'DashedRoute']);
-});
