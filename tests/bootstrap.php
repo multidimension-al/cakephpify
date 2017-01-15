23b34327104c54f2416965b914207fc74a1f48ec
AJ Quick <ajquick@gmail.com>
2017-01-14 17:28:41 -0800

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,20 +19,6 @@ use Cake\Datasource\ConnectionManager;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         3.0.0
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
@@ -42,6 +28,7 @@ define('APP_DIR', 'src');
 define('APP', ROOT . DS . APP_DIR . DS);
 define('CONFIG', ROOT . DS . 'config' . DS);
 define('WWW_ROOT', ROOT . DS . 'webroot' . DS);
+define('WEBROOT_DIR', 'webroot');
 define('TESTS', ROOT . DS . 'tests' . DS);
 define('TMP', ROOT . DS . 'tmp' . DS);
 define('LOGS', ROOT . DS . 'logs' . DS);
@@ -50,34 +37,34 @@ define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'c
 define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
 define('CAKE', CORE_PATH . 'src' . DS);
 
-
-define('WEBROOT_DIR', 'webroot');
-
-require_once CAKE_CORE_INCLUDE_PATH . DS . 'config' . DS . 'bootstrap.php';
+require_once CORE_PATH . 'config' . DS . 'bootstrap.php';
 
 date_default_timezone_set('UTC');
 mb_internal_encoding('UTF-8');
 
 Configure::write('debug', true);
+
 Configure::write('App', [
     'namespace' => 'App',
-    'encoding' => 'UTF-8',
+    'encoding' => env('APP_ENCODING', 'UTF-8'),
+    'defaultLocale' => env('APP_DEFAULT_LOCALE', 'en_US'),
     'base' => false,
-    'baseUrl' => false,
     'dir' => 'src',
     'webroot' => WEBROOT_DIR,
-    'www_root' => WWW_ROOT,
-    'fullBaseUrl' => 'http://localhost',
+    'wwwRoot' => WWW_ROOT,
+    // 'baseUrl' => env('SCRIPT_NAME'),
+    'fullBaseUrl' => false,
     'imageBaseUrl' => 'img/',
-    'jsBaseUrl' => 'js/',
     'cssBaseUrl' => 'css/',
+    'jsBaseUrl' => 'js/',
     'paths' => [
-        'plugins' => [dirname(APP) . DS . 'plugins' . DS],
-        'templates' => [APP . 'Template' . DS]
-    ]
+        'plugins' => [ROOT . DS . 'plugins' . DS],
+        'templates' => [APP . 'Template' . DS],
+        'locales' => [APP . 'Locale' . DS],
+    ],
 ]);
 
-Plugin::load('Multidimensional/Shopify', ['path' => ROOT, 'routes' => true]);
+Plugin::load('Multidimensional/Shopify', ['path' => ROOT, 'autoload' => true]);
 
 if (!getenv('DB_DSN')) {
     putenv('DB_DSN=sqlite:///:memory:');
