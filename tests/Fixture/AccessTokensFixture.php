c1f18ebb25cb0b76546efd9855c20c2e3205de8c
AJ Quick <ajquick@gmail.com>
2017-01-14 21:46:20 -0800

    Update AccessTokensFixture.php
    Postgres code needs datetime to be 'null' => true.
    
    This is a work around until a bug can be fixed.

--- tests/Fixture/AccessTokensFixture.php
+++ tests/Fixture/AccessTokensFixture.php
@@ -24,8 +24,8 @@ class AccessTokensFixture extends TestFixture {
         'shop_id' => ['type' => 'integer', 'length' => 10, 'null' => false],
         'api_key' => ['type' => 'string', 'length' => 32, 'null' => false],
         'token' => ['type' => 'string', 'length' => 255, 'null' => false],
-        'created_at' => ['type' => 'datetime', 'default' => 'CURRENT_TIMESTAMP', 'null' => false],
-        'updated_at' => ['type' => 'datetime', 'default' => 'CURRENT_TIMESTAMP', 'null' => false],
+        'created_at' => ['type' => 'datetime', 'default' => 'CURRENT_TIMESTAMP', 'null' => true],
+        'updated_at' => ['type' => 'datetime', 'default' => 'CURRENT_TIMESTAMP', 'null' => true],
         'expired_at' => ['type' => 'datetime', 'null' => true],
         '_constraints' => [
             'PRIMARY' => ['type' => 'primary', 'columns' => ['id']],
