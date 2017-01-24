0821946154aa60ed6ac1f3c765bb757b9e8bdbad
AJ Quick <ajquick@gmail.com>
2017-01-23 22:22:53 -0700

    Update Migrations
    Remove namespace.

--- config/Migrations/20161216042048_Database.php
+++ config/Migrations/20161216042048_Database.php
@@ -13,8 +13,6 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-namespace Multidimensional\Cakephpify\Migrations;
-
 use Migrations\AbstractMigration;
 
 class Database extends AbstractMigration
--- src/Shell/ShopifyInstallShell.php
+++ src/Shell/ShopifyInstallShell.php
@@ -48,26 +48,26 @@ class ShopifyInstallShell extends Shell
 
             $this->out();
 
-            $scopeArray =
-                ['read_content',
-                 'write_content',
-                 'read_themes',
-                 'write_themes',
-                 'read_products',
-                 'write_products',
-                 'read_customers',
-                 'write_customers',
-                 'read_orders',
-                 'write_orders',
-                 'read_script_tags',
-                 'write_script_tags',
-                 'read_fulfillments',
-                 'write_fulfillments',
-                 'read_shipping',
-                 'write_shipping',
-                 'read_analytics',
-                 'read_users',
-                 'write_users'];
+            $scopeArray = [
+                'read_content',
+                'write_content',
+                'read_themes',
+                'write_themes',
+                'read_products',
+                'write_products',
+                'read_customers',
+                'write_customers',
+                'read_orders',
+                'write_orders',
+                'read_script_tags',
+                'write_script_tags',
+                'read_fulfillments',
+                'write_fulfillments',
+                'read_shipping',
+                'write_shipping',
+                'read_analytics',
+                'read_users',
+                'write_users'];
 
             $this->out('Enter your application\'s scope here.', 2);
             $this->out('Valid scope options:', 2);
